0ab3e994a9fa710d6b9bf0d3ff86b9fa7a60b9cd
Daniel Machado <daniel.cdedicado@gmail.com>
2026-09-01 10:06:46 -0300

    iniciando estudo

---
+++ curso-de-php/index.php
@@ -0,0 +1,14 @@
+<?php
+
+declare (strict_types=1);
+
+$urls = parse_url("https://google.com/");
+
+var_dump($urls);
+
+$today = 1;
+
+$message = "bem vindo ao dia  " . $today;
+
+echo $message;
+?>
