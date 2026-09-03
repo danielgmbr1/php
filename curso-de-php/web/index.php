f339d43992dcd3d7a613b2e99d3904a401522fd2
Daniel Machado <daniel.cdedicado@gmail.com>
2026-09-03 13:56:16 -0300

    commit

--- curso-de-php/web/index.php
+++ curso-de-php/web/index.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 var_dump(hash_file('sha256', 'Final_Mace_Descent.mp3'));
 
-
-$hash = password_hash('1234', PASSWORD_ARGON2ID);
+$hash = password_hash('123', PASSWORD_ARGON2ID);
 
 var_dump(password_verify('123', $hash));
+
+echo " 🚀";
