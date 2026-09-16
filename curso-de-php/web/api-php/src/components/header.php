17257c08e632c70ce071e6508e5ce40434925aec
Daniel Machado <daniel.cdedicado@gmail.com>
2026-09-16 13:57:46 -0300

    commit

--- curso-de-php/web/api-php/src/components/header.php
+++ curso-de-php/web/api-php/src/components/header.php
@@ -11,6 +11,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
+    <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/fontawesome/css/all.min.css" rel="stylesheet">
+    <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    
+     
--- curso-de-php/web/api-php/src/pages/home.php
+++ curso-de-php/web/api-php/src/pages/home.php
@@ -1,1 +1,7 @@
 <p>Página inicial</p>
+<p>
+    <a href="/sobre" title="Ir para a página sobre" class="btn btn-primary btn-sm">
+    <i class="fa-solid fa-trophy"></i>
+    Sobre
+    </a>
+</p>
